cfabaed539742fe87b3138bfd05b8bfcf1253c8d
Art <cipherwraith666@gmail.com>
2013-11-15 11:57:06 +0800

    Create CSS.hs

---
+++ CSS.hs
@@ -0,0 +1,22 @@
+module CSS where
+import Data.List
+import System.Directory
+import System.FilePath
+import Config
+
+outputCSS :: IO ()
+outputCSS = do
+  cssFiles <- getCSSFiles
+  let c = length cssFiles
+  mapM_ writeCSS cssFiles
+
+writeCSS (path, content) = writeFile outPath content
+  where
+    outPath = replaceDirectory path htmlDirectory
+
+getCSSFiles :: IO [(String, String)]
+getCSSFiles = do
+  dirContents <- getDirectoryContents cssDirectory
+  let files = map (cssDirectory </>) . delete "." . delete ".." $ dirContents
+  fileContent <- mapM readFile files
+  return $ zip files fileContent
