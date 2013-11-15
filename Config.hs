c2f8c0e84af6a11facd3ddde45170570c751825a
Art <cipherwraith666@gmail.com>
2013-11-15 14:30:30 +0800

    Update Config.hs

--- Config.hs
+++ Config.hs
@@ -1,9 +1,12 @@
 module Config where
 import System.FilePath
 
+siteDomain = "http://www.asdf.com/"
 siteDirectory = "/Users/asdf/Code/sandbox/haskellblog"
 postsDirectory = siteDirectory </> "posts"
 htmlDirectory = siteDirectory </> "html"
+staticDirectory = siteDomain </> "static" 
+localStaticDirectory = htmlDirectory </> "static"
 rssDirectory = htmlDirectory </> "rss"
 templatesDirectory = siteDirectory </> "templates"
 indexLocation = htmlDirectory </> "index" <.> "html"
