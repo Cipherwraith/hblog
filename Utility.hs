dc5f7a393d87b6df96e8f109d4e6d0f036e448a7
Art <cipherwraith666@gmail.com>
2013-11-15 16:48:01 +0800

    Update Utility.hs

--- Utility.hs
+++ Utility.hs
@@ -1,5 +1,7 @@
 module Utility where
 
+import Data.Time
+import Data.Time.Clock.POSIX
 import qualified Data.Text.Lazy as T
 import Data.Char
 
@@ -9,4 +11,5 @@ removeWhitespace = T.pack . filter (not . isSpace) . T.unpack
 removeNonAlphaNum :: T.Text -> T.Text
 removeNonAlphaNum = T.pack . filter (isAlphaNum) . T.unpack
 
+convertTimeToString x = posixSecondsToUTCTime ((fromInteger x) :: POSIXTime)
 
