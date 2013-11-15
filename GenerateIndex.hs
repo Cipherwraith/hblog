b9d0627581c94ad839c5a69d303856cdbc42096d
Art <cipherwraith666@gmail.com>
2013-11-15 14:29:31 +0800

    Update GenerateIndex.hs

--- GenerateIndex.hs
+++ GenerateIndex.hs
@@ -1,20 +1,25 @@
 module GenerateIndex where
+import Config
 import Templates
 import Types
 import Data.Monoid
 import Data.List
+import RecentPosts
 
 import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
 
-generateIndex :: [Publish] -> IO T.Text
-generateIndex publishData = do
+generateIndex :: [Publish] -> T.Text -> IO T.Text
+generateIndex publishData recent = do
   htmlHead <- headerRaw
   htmlFoot <- footerRaw
-  return $ mconcat [htmlHead, content, htmlFoot]
+  return $ mconcat [htmlHead, recent, content, htmlFoot]
  where
   content :: T.Text
   content = mconcat $ map _content . take 10 . reverse . sort $ publishData
   
-
+writeIndex publish recent = do
+  indexContent <- generateIndex publish recent
+  T.writeFile indexLocation indexContent
 
 
